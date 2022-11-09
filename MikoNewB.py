d01b9c184b206a0ef88a93e9302eebf2263e1ab9
Madikosha <migibayeva@griddynamics.com>
2022-11-09 14:30:50 +0100

    updating Miko

--- MikoNewB.py
+++ MikoNewB.py
@@ -3,3 +3,10 @@ for i in ("somwehritd text one count"):
     if i =="o":
         j+=1
         print ("I am printing o - "+str(j))
+
+
+j=5
+for i in ("soheresisodkfnmnsd sdflkvnmsld spdjfqwe,ads';,cghnewm'lam,sfdfgnm;dfh"):
+    if i in (",:l;")
+        j+=1
+        print ("Another characters o - "+str(j))
